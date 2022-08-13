c2c086af53119adc8a977c973e9c1b598a9a3d51
Ayan Akkassov <akkassov.ayan@gmail.com>
2022-08-14 05:15:49 +0600

    Add search handler

---
+++ provider_b/app/api/v1/search.py
@@ -0,0 +1,11 @@
+import asyncio
+
+from aiohttp import web
+
+from app.utils import parsers
+
+
+async def handle(request: web.Request) -> web.Response:
+    data = parsers.JsonParser.parse('response_b.json')
+    # await asyncio.sleep(60)
+    return web.json_response({'data': data})
